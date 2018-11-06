3a7eb545be43a31430746c132e6ccaf230ed9afd
Alangi Derick <alangiderick@gmail.com>
2018-11-06 16:48:49 +0100

    Stubbing a Class using PHPUnit
    StubTest.php - create a stub from the Stub class using the
    createMock() method provided by PHPUnit.

---
+++ chapter_nine/Stub.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlangiDerick
+ * Date: 06/11/2018
+ * Time: 16:02
+ */
+
+class Stub
+{
+	public function doSomething()
+	{
+		// Do something :)
+	}
+}
---
+++ chapter_nine/StubTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlangiDerick
+ * Date: 06/11/2018
+ * Time: 16:02
+ */
+
+require 'Stub.php';
+
+use PHPUnit\Framework\TestCase;
+
+class StubTest extends TestCase
+{
+	public function testStub()
+	{
+		// Create a stub for the Stub class.
+		$stub = $this->createMock( Stub::class );
+
+		// Configure the stub.
+		$stub->method('doSomething')
+			 ->willReturn( 'foo' );
+
+		// Calling $stub->doSomething() will now return 'foo'
+		$this->assertSame( 'foo', $stub->doSomething() );
+	}
+}
