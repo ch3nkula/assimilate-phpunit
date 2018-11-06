2555ac7d9d365aa9bb1accfde8ebb90b939fe017
Alangi Derick <alangiderick@gmail.com>
2018-11-06 16:59:07 +0100

    Use the PHPUnit Mock Builder API to create stubs
    In this case, instead of using createMock() method provided
    by PHPUnit, use the PHPUnit Mock Builder API to create stubs.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_nine/StubMockTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlangiDerick
+ * Date: 06/11/2018
+ * Time: 16:50
+ */
+
+use PHPUnit\Framework\TestCase;
+
+require 'Stub.php';
+
+class StubMockTest extends TestCase
+{
+	public function testStub()
+	{
+		// Create a stub for the Stub class.
+		$stub = $this->getMockBuilder( Stub::class )
+					 ->disableOriginalConstructor()
+					 ->disableOriginalClone()
+					 ->disableArgumentCloning()
+					 ->disallowMockingUnknownTypes()
+					 ->getMock();
+
+		// Configure the stub.
+		$stub->method('doSomething')
+			 ->willReturn( 'foo' );
+
+		// Calling $stub->doSomething() will now return 'foo'
+		$this->assertSame( 'foo', $stub->doSomething() );
+	}
+}
