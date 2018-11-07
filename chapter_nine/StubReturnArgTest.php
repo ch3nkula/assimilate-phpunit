748e419b5cbbe66837e5e01a206dd1b3c728a060
Alangi Derick <alangiderick@gmail.com>
2018-11-07 11:08:24 +0100

    Stub with a dynamic return argument
    StubReturnArgTest.php - multiple tests with different
    return argument values.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_nine/StubReturnArgTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlangiDerick
+ * Date: 07/11/2018
+ * Time: 10:59
+ */
+
+require 'Stub.php';
+
+use PHPUnit\Framework\TestCase;
+
+class StubReturnArgTest extends TestCase
+{
+	public function testReturnArgumentStub()
+	{
+		// Create a stub for the Stub class.
+		$stub = $this->createMock( Stub::class );
+
+		// Configure the stub.
+		$stub->method( 'doSomething' )
+			 ->will( $this->returnArgument( 0 ) );
+
+		// $stub->doSomething( 'foo' ) returns 'foo'
+		$this->assertSame( 'foo', $stub->doSomething( 'foo' ) );
+
+		// $stub->doSomething( 'bar' ) returns 'bar'
+		$this->assertSame( 'bar', $stub->doSomething( 'bar' ) );
+	}
+}
