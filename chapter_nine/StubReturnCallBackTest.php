0a68f125ca0cac21030172b9a8e1abb8d167e4b8
Alangi Derick <alangiderick@gmail.com>
2018-11-10 14:19:26 +0100

    Returning a callback function in a stub
    $this->returnCallback() does the trick :)
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

---
+++ chapter_nine/StubReturnCallBackTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlangiDerick
+ * Date: 09/11/2018
+ * Time: 17:45
+ */
+
+require 'Stub.php';
+
+use PHPUnit\Framework\TestCase;
+
+class StubReturnCallBackTest extends TestCase
+{
+	public function testReturnCallbackStub(){
+		// Create a stub for the Stub class.
+		$stub = $this->createMock( Stub::class );
+
+		// Configure the stub.
+		$stub->method( 'doSomething' )
+			->will( $this->returnCallback( 'str_rot13' ) );
+
+		// $stub->doSomething( $argument ) returns str_rot13( $argument )
+		$this->assertSame( 'fbzrguvat', $stub->doSomething( 'something' ) );
+	}
+}
